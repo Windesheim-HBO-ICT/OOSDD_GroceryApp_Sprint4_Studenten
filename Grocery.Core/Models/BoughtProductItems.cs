eedba9d9d14d05db0f8833d19c1dfca8b09abea9
Bryan Li <s1186487@student.windesheim.nl>
2025-10-01 17:13:10 +0200

    Start gemaakt aan UC13 deze zit nog in de beginfase

---
+++ Grocery.Core/Models/BoughtProductItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.Core.Models
+{
+    public class BoughtProductItem
+    {
+        public Client Client { get; set; }
+        public GroceryList GroceryList { get; set; }
+        public Product Product { get; set; } 
+    }
+}
---
+++ Grocery.Core/Models/BoughtProductItemsviewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+using Grocery.Core.Services;
+
+namespace Grocery.App.ViewModels
+{
+    public partial class BoughtProductsViewModel : ObservableObject
+    {
+        private readonly BoughtProductsService _service;
+
+        [ObservableProperty]
+        private ObservableCollection<BoughtProductItem> products = new();
+
+        public BoughtProductsViewModel(BoughtProductsService service)
+        {
+            _service = service;
+        }
+
+        public void OnSelectedProductChanged(int productId)
+        {
+            var items = _service.Get(productId)
+                .Select(x => new BoughtProductItem
+                {
+                    Client = x.Client,
+                    GroceryList = x.GroceryList,
+                    Product = x.Product 
+                })
+                .ToList();
+
+            Products = new ObservableCollection<BoughtProductItem>(items);
+        }
+    }
+}
