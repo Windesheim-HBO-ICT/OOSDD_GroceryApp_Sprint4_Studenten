0a4f2b49652a46e9e304eaac759194e733ba5db7
Sem <s1207692@student.windesheim.nl>
2025-10-03 16:23:19 +0200

    Implementatie UC 10, 11 en 13.

--- Grocery.Core/Services/BoughtProductsService.cs
+++ Grocery.Core/Services/BoughtProductsService.cs
@@ -1,7 +1,8 @@
-﻿
-using Grocery.Core.Interfaces.Repositories;
+﻿using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Grocery.Core.Services
 {
@@ -11,16 +12,50 @@ namespace Grocery.Core.Services
         private readonly IClientRepository _clientRepository;
         private readonly IProductRepository _productRepository;
         private readonly IGroceryListRepository _groceryListRepository;
-        public BoughtProductsService(IGroceryListItemsRepository groceryListItemsRepository, IGroceryListRepository groceryListRepository, IClientRepository clientRepository, IProductRepository productRepository)
+
+        public BoughtProductsService(
+            IGroceryListItemsRepository groceryListItemsRepository,
+            IGroceryListRepository groceryListRepository,
+            IClientRepository clientRepository,
+            IProductRepository productRepository)
         {
-            _groceryListItemsRepository=groceryListItemsRepository;
-            _groceryListRepository=groceryListRepository;
-            _clientRepository=clientRepository;
-            _productRepository=productRepository;
+            _groceryListItemsRepository = groceryListItemsRepository;
+            _groceryListRepository = groceryListRepository;
+            _clientRepository = clientRepository;
+            _productRepository = productRepository;
         }
+
         public List<BoughtProducts> Get(int? productId)
         {
-            throw new NotImplementedException();
+            if (productId == null) return new List<BoughtProducts>();
+
+            var result = new List<BoughtProducts>();
+
+            // Alle clients ophalen
+            var allClients = _clientRepository.GetAll();
+
+            foreach (var client in allClients)
+            {
+                // Voor elke boodschappenlijst van de client
+                var lists = _groceryListRepository.GetByClientId(client.Id);
+                if (lists == null) continue;
+
+                foreach (var list in lists)
+                {
+                    var items = _groceryListItemsRepository.GetByListId(list.Id);
+                    if (items == null) continue;
+
+                    foreach (var item in items)
+                    {
+                        if (item.Product != null && item.Product.Id == productId)
+                        {
+                            result.Add(new BoughtProducts(client, list, item.Product));
+                        }
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
