8ef0c2a9196f2ef720213c4d7ccb331fb692c826
ernestoheres <ernestoheres09@gmail.com>
2025-10-03 10:47:00 +0200

    Fix(TestCore): file niet goed meegegeven

--- TestCore/TestHelpers.cs
+++ TestCore/TestHelpers.cs
@@ -39,9 +39,11 @@ namespace TestCore
 
         [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08")]
         [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA")]
-        public void TestPasswordHelperReturnsFalse(string password, string passwordHash)
+        public void TestPasswordHelperThrowsOnInvalid(string password, string passwordHash)
         {
-
+            // hash is niet in de goede formt
+            Assert.Throws<FormatException>(() =>
+                  PasswordHelper.VerifyPassword(password, passwordHash));
         }
     }
 }
